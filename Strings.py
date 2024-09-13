53eb7496e68662727ff67476b5d728a12dd7dbb8
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 00:26:55 -0600

    Practica 2

---
+++ Strings.py
@@ -0,0 +1,12 @@
+# Definimos una variable llamada 'mensaje01' que almacena el texto "Buenos dias".
+mensaje01 = 'Buenos dias'
+
+# Definimos otra variable llamada 'mensaje02' que almacena el texto "Buenas noches".
+mensaje02 = "Buenas noches"
+
+# Definimos una variable llamada 'funcionprint' que guarda una explicación sobre la función 'print()'.
+# Esta explicación indica que la función 'print()' muestra en la consola el contenido de una variable o valor.
+funcionprint = '"print()" es una funcion que le imprime al usuario una variable seleccionada en la consola'
+
+# Usamos la función 'print()' para mostrar el contenido de la variable 'funcionprint' en la consola.
+print(funcionprint)
