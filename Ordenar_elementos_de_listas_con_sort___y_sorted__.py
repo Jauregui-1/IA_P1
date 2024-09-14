573c923228db28a699f9827615b4e87757b6f06b
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 22:43:46 -0600

    Practica 16

---
+++ Ordenar_elementos_de_listas_con_sort___y_sorted__.py
@@ -0,0 +1,9 @@
+# Se crea una lista de colores con 10 elementos
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se utiliza el método sort() con el argumento reverse=True para ordenar la lista en orden descendente
+# Esto reorganiza la lista de colores de la Z a la A
+colores.sort(reverse=True)
+
+# Se imprime la lista 'colores' después de haber sido ordenada en orden descendente
+print(colores)
