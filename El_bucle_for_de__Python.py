d6198fa204a15b63f6069c317c18adb2e7a42b2b
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:05:54 -0600

    Practica 26

---
+++ El_bucle_for_de__Python.py
@@ -0,0 +1,7 @@
+# Se crea una lista de tipos de tacos
+tacos = ['PASTOR', 'SUADERO', 'TRIPA', 'CHORIZO', 'ASADA', 'LENGUA']
+
+# Se inicia un bucle for para iterar sobre cada elemento en la lista 'tacos'
+for x in tacos:
+    # En cada iteración, se imprime el tipo de taco actual con un mensaje
+    print('El taco es de:', x, '.')
