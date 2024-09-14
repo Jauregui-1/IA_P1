8c1c712d631ee07bf82479497fcd88338ac2f684
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:16:41 -0600

    Practica 5

--- Saltos_de_linea_y_tabulaciones.py
+++
@@ -1,3 +0,0 @@
-# Se imprime una cadena de texto que contiene varias palabras con formato de salto de línea y tabulación.
-# El método .title() convierte la primera letra de cada palabra en mayúscula y el resto en minúscula.
-print("\n\n\tpython\n\n\tjavaScript\n\n\tjava\n\n\tpHP\n\n\ttypeScript\n\n\tsQL\n\n\tcOBOL".title())
