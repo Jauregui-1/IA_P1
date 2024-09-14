505075412c2d1a5d0e1ae4c0a6963e6767b31cd5
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 22:21:33 -0600

    Practica 7

---
+++ Exponentes.py
@@ -0,0 +1,5 @@
+# Se realiza la multiplicación de 2 por sí mismo 10 veces, lo cual es equivalente a 2 elevado a la potencia 10
+mil_veinticuatro = 2*2*2*2*2*2*2*2*2*2
+
+# Se imprime el valor resultante de la multiplicación, que es 1024
+print(mil_veinticuatro)
