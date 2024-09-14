8180efed879d2af8e4818e78cb2c164d01a11f8d
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 22:51:47 -0600

    Practica 19

---
+++ Condicional_IF_y_operadores_de_comparación.py
@@ -0,0 +1,26 @@
+# Se definen dos variables num1 y num2 con valores 15 y 20, respectivamente
+num1 = 15
+num2 = 20
+
+# Se evalúa una condición: si num1 es menor que num2
+# En este caso, 15 es menor que 20, por lo que se ejecuta el bloque del if
+if num1 < num2:
+    print('Se ejecuta el if.')
+
+# Se definen dos nuevas variables num1 y num2 con valores 1450 y 60, respectivamente
+num1 = 1450
+num2 = 60
+
+# Se evalúa una condición: si num1 es mayor que num2
+# En este caso, 1450 es mayor que 60, por lo que se ejecuta el bloque del if
+if num1 > num2:
+    print('Se ejecuta el if.')
+
+# Se definen las mismas variables num1 y num2 con los valores 1450 y 60
+num1 = 1450
+num2 = 60
+
+# Se evalúa una condición: si num1 es igual a num2
+# En este caso, 1450 no es igual a 60, por lo que el bloque del if no se ejecuta
+if num1 == num2:
+    print('Se ejecuta el if.')
