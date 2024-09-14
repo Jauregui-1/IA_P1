9001afdbbd85d7a1f0cd94ebfef048e516adfa2b
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:00:07 -0600

    Practica 23

---
+++ Múltiples_condiciones_if.py
@@ -0,0 +1,33 @@
+# Se solicita al usuario que ingrese una letra
+# La entrada del usuario se almacena en la variable 'pregunta'
+pregunta = input('Introduce una de las primeras 6 letras del abecedario: \n')
+
+# Se verifica si la letra ingresada es igual a 'a'
+if pregunta == 'a':
+    # Si la letra es 'a', se imprime un mensaje indicando que se escribió la letra 'a'
+    print('Escribiste la letra a.')
+
+# Se verifica si la letra ingresada es igual a 'b'
+if pregunta == 'b':
+    # Si la letra es 'b', se imprime un mensaje indicando que se escribió la letra 'b'
+    print('Escribiste la letra b.')
+
+# Se verifica si la letra ingresada es igual a 'c'
+if pregunta == 'c':
+    # Si la letra es 'c', se imprime un mensaje indicando que se escribió la letra 'c'
+    print('Escribiste la letra c.')
+
+# Se verifica si la letra ingresada es igual a 'd'
+if pregunta == 'd':
+    # Si la letra es 'd', se imprime un mensaje indicando que se escribió la letra 'd'
+    print('Escribiste la letra d.')
+
+# Se verifica si la letra ingresada es igual a 'e'
+if pregunta == 'e':
+    # Si la letra es 'e', se imprime un mensaje indicando que se escribió la letra 'e'
+    print('Escribiste la letra e.')
+
+# Se verifica si la letra ingresada es igual a 'f'
+if pregunta == 'f':
+    # Si la letra es 'f', se imprime un mensaje indicando que se escribió la letra 'f'
+    print('Escribiste la letra f.')
