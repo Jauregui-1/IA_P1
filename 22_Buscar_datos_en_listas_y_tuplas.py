b13a33c981870970c660772154a3f9402f448022
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:24:03 -0600

    Practica 22

--- 22_Buscar_datos_en_listas_y_tuplas.py
+++ 22_Buscar_datos_en_listas_y_tuplas.py
@@ -5,10 +5,10 @@ colores = ('rojo', 'negro', 'blanco', 'azul')
 # La entrada del usuario se almacena en la variable 'pregunta'
 pregunta = input('Que color buscas?: ')
 
-# Se verifica si el color ingresado por el usuario est· en la tupla 'colores'
+# Se verifica si el color ingresado por el usuario est√° en la tupla 'colores'
 if pregunta in colores:
-    # Si el color est· en la tupla, se imprime un mensaje indicando que el color est· incluido
+    # Si el color est√° en la tupla, se imprime un mensaje indicando que el color est√° incluido
     print('El color:', pregunta, 'esta incluido.')
 else:
-    # Si el color no est· en la tupla, se imprime un mensaje indicando que el color no est· incluido
+    # Si el color no est√° en la tupla, se imprime un mensaje indicando que el color no est√° incluido
     print('El color:', pregunta, 'no esta incluido.')
