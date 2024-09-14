4a3cc51f516c6f4019eb93bdc6cd47637d186c99
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:17:45 -0600

    Practica 8

---
+++ 08_Floats_y_el_método_round__.py
@@ -0,0 +1,6 @@
+# Se asigna un número flotante con muchas cifras decimales a la variable 'numero'
+numero = 17.567383292929200234
+
+# El método round() redondea el número a 5 decimales
+# El primer argumento es el número que queremos redondear y el segundo es la cantidad de decimales
+print(round(numero, 5))
