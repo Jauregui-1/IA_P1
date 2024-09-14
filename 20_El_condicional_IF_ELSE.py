f5788319b72163375b60e83aefb3dd33e4a43035
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:23:27 -0600

    Practica 20

--- 20_El_condicional_IF_ELSE.py
+++ 20_El_condicional_IF_ELSE.py
@@ -1,10 +1,10 @@
 # Se asigna la cadena 'ROJO' a la variable 'color'
 color = 'ROJO'
 
-# Se evalúa una condición: si 'color' es igual a 'ROJO'
+# Se evalÃºa una condiciÃ³n: si 'color' es igual a 'ROJO'
 # En este caso, 'color' es igual a 'ROJO', por lo que se ejecuta el bloque del if
 if color == 'ROJO':
     print('El color es ROJO')
-# Si 'color' no fuera igual a 'ROJO', se ejecutaría el bloque del else
+# Si 'color' no fuera igual a 'ROJO', se ejecutarÃ­a el bloque del else
 else:
     print('El color no es ROJO')
