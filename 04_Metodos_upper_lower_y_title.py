b7f9ffbba1eefcb14b414bdf609693b35cb2f805
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:16:22 -0600

    Practica 4

--- 04_Metodos_upper_lower_y_title.py
+++ 04_Metodos_upper_lower_y_title.py
@@ -1,19 +1,19 @@
 # Asignamos una cadena de texto con un nombre a la variable 'nombre'
 nombre = 'enrique barros fernandez '
 
-# El método .title() convierte la primera letra de cada palabra en mayúscula
+# El mÃ©todo .title() convierte la primera letra de cada palabra en mayÃºscula
 nombre = nombre.title()
 
 # Asignamos una cadena de texto a la variable 'frase'
 frase = '\nEsta Es Una Frase Para Ser Formateada.'
 
-# El método .lower() convierte toda la cadena de texto a minúsculas
+# El mÃ©todo .lower() convierte toda la cadena de texto a minÃºsculas
 frase = frase.lower()
 
 # Asignamos otra cadena de texto a la variable 'frase01'
 frase01 = '\nEsta Es Una Frase Para Ser Formateada.'
 
-# El método .upper() convierte toda la cadena de texto a mayúsculas
+# El mÃ©todo .upper() convierte toda la cadena de texto a mayÃºsculas
 frase01 = frase01.upper()
 
 # Usamos print() para imprimir las variables 'nombre', 'frase' y 'frase01'
