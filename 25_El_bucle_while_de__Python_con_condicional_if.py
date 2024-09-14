1c28a9da915057bfe947adacf33cf8d7501e83f8
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:25:01 -0600

    Practica 25

--- 25_El_bucle_while_de__Python_con_condicional_if.py
+++ 25_El_bucle_while_de__Python_con_condicional_if.py
@@ -5,19 +5,19 @@ x = 0
 while x <= 30:
     # Se imprime el valor actual de x
     print('El valor del bucle es: ', x)
-    # Se incrementa x en 1 en cada iteración del bucle
+    # Se incrementa x en 1 en cada iteraciÃ³n del bucle
     x += 1
     
     # Se verifica si x es igual a 4, 6 o 10
     if x == 4 or x == 6 or x == 10:
-        # Si x es igual a uno de estos valores, se imprime un mensaje indicando que se saltó el valor
+        # Si x es igual a uno de estos valores, se imprime un mensaje indicando que se saltÃ³ el valor
         print('Se salto el valor: ', x, 'de X')
-        # La instrucción continue omite el resto del código en el bucle y pasa a la siguiente iteración
+        # La instrucciÃ³n continue omite el resto del cÃ³digo en el bucle y pasa a la siguiente iteraciÃ³n
         continue
     
     # Se verifica si x es igual a 15
     if x == 15:
-        # Si x es igual a 15, se imprime un mensaje indicando que el bucle se rompió
+        # Si x es igual a 15, se imprime un mensaje indicando que el bucle se rompiÃ³
         print('Se rompio el bucle estando en: ', x, 'de X')
-        # La instrucción break rompe el bucle, saliendo de él inmediatamente
+        # La instrucciÃ³n break rompe el bucle, saliendo de Ã©l inmediatamente
         break
