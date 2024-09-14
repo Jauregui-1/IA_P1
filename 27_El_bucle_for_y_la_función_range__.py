2e21f761aac7112459582d7ddbf454d46ceab9fb
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:25:40 -0600

    Practica 27

--- 27_El_bucle_for_y_la_función_range__.py
+++ 27_El_bucle_for_y_la_función_range__.py
@@ -1,5 +1,5 @@
-# Se inicia un bucle for que itera sobre un rango de números
+# Se inicia un bucle for que itera sobre un rango de nÃºmeros
 # El rango comienza en 7, termina antes de 700, y se incrementa en pasos de 100
 for x in range(7, 700, 100):
-    # En cada iteración, se imprime el valor actual de x
+    # En cada iteraciÃ³n, se imprime el valor actual de x
     print(x)
