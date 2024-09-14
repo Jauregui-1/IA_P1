3d9acb8d1fe7998e211ca702fb827e5d4301ace0
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:08:35 -0600

    Practica 28

---
+++ Qué_son_los_diccionarios.py
@@ -0,0 +1,19 @@
+# Se crea un diccionario llamado 'teclado1' que contiene información sobre un teclado
+# Las claves del diccionario son 'CATEGORIA', 'MODELO', y 'PRECIO'
+teclado1 = {
+    'CATEGORIA': 'TECLADOS',
+    'MODELO': 'HYPERX ALLOY FPS PRO',
+    'PRECIO': '89.99'
+}
+
+# Se crea un segundo diccionario llamado 'teclado2' con información sobre otro teclado
+# Las claves del diccionario son 'CATEGORIA', 'MODELO', y 'PRECIO'
+teclado2 = {
+    'CATEGORIA': 'TECLADOS',
+    'MODELO': 'CORSAIR K55 RGB',
+    'PRECIO': '59.99'
+}
+
+# Se imprime el modelo y el precio del teclado2
+# Se accede a los valores del diccionario 'teclado2' usando las claves 'MODELO' y 'PRECIO'
+print('El modelo: ', teclado2['MODELO'], 'tiene un precio de: ', teclado2['PRECIO'])
