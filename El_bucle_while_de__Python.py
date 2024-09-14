280a62be946e8f32548ffb7cbbb27c749ca885bc
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:02:26 -0600

    Practica 24

---
+++ El_bucle_while_de__Python.py
@@ -0,0 +1,41 @@
+# Se imprime un mensaje indicando el inicio del primer bucle while
+print('Primer bucle while')
+
+# Se inicializa la variable x con valor 0
+x = 0
+# Se inicia un bucle while que se ejecuta mientras x sea menor o igual a 15
+while x <= 15:
+    # Se imprime el valor actual de x
+    print(x)
+    # Se incrementa x en 5 en cada iteración del bucle
+    x += 5
+
+# Se imprime una línea en blanco para separar las salidas de los bucles
+print('\n')
+
+# Se imprime un mensaje indicando el inicio del segundo bucle while
+print('Segundo bucle while')
+
+# Se inicializa la variable x con valor 0
+x = 0
+# Se inicia un bucle while que se ejecuta mientras x sea mayor o igual a -100
+while x >= -100:
+    # Se imprime el valor actual de x
+    print(x)
+    # Se decrementa x en 20 en cada iteración del bucle
+    x -= 20
+
+# Se imprime una línea en blanco para separar las salidas de los bucles
+print('\n')
+
+# Se imprime un mensaje indicando el inicio del tercer bucle while
+print('Tercer bucle while')
+
+# Se inicializa la variable x con valor 10
+x = 10
+# Se inicia un bucle while que se ejecuta mientras x sea mayor o igual a 0
+while x >= 0:
+    # Se imprime el valor actual de x
+    print(x)
+    # Se decrementa x en 1 en cada iteración del bucle
+    x -= 1
