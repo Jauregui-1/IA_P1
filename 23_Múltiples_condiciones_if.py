a4ed2154bd408c72a6859ce169dd208063cd5fe0
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:24:22 -0600

    Practica 23

--- 23_Múltiples_condiciones_if.py
+++ 23_Múltiples_condiciones_if.py
@@ -4,30 +4,30 @@ pregunta = input('Introduce una de las primeras 6 letras del abecedario: \n')
 
 # Se verifica si la letra ingresada es igual a 'a'
 if pregunta == 'a':
-    # Si la letra es 'a', se imprime un mensaje indicando que se escribió la letra 'a'
+    # Si la letra es 'a', se imprime un mensaje indicando que se escribiÃ³ la letra 'a'
     print('Escribiste la letra a.')
 
 # Se verifica si la letra ingresada es igual a 'b'
 if pregunta == 'b':
-    # Si la letra es 'b', se imprime un mensaje indicando que se escribió la letra 'b'
+    # Si la letra es 'b', se imprime un mensaje indicando que se escribiÃ³ la letra 'b'
     print('Escribiste la letra b.')
 
 # Se verifica si la letra ingresada es igual a 'c'
 if pregunta == 'c':
-    # Si la letra es 'c', se imprime un mensaje indicando que se escribió la letra 'c'
+    # Si la letra es 'c', se imprime un mensaje indicando que se escribiÃ³ la letra 'c'
     print('Escribiste la letra c.')
 
 # Se verifica si la letra ingresada es igual a 'd'
 if pregunta == 'd':
-    # Si la letra es 'd', se imprime un mensaje indicando que se escribió la letra 'd'
+    # Si la letra es 'd', se imprime un mensaje indicando que se escribiÃ³ la letra 'd'
     print('Escribiste la letra d.')
 
 # Se verifica si la letra ingresada es igual a 'e'
 if pregunta == 'e':
-    # Si la letra es 'e', se imprime un mensaje indicando que se escribió la letra 'e'
+    # Si la letra es 'e', se imprime un mensaje indicando que se escribiÃ³ la letra 'e'
     print('Escribiste la letra e.')
 
 # Se verifica si la letra ingresada es igual a 'f'
 if pregunta == 'f':
-    # Si la letra es 'f', se imprime un mensaje indicando que se escribió la letra 'f'
+    # Si la letra es 'f', se imprime un mensaje indicando que se escribiÃ³ la letra 'f'
     print('Escribiste la letra f.')
