522f348741bd17fce554cc1d071612966dc00b52
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 22:39:05 -0600

    Practica 14

---
+++ Insertar_elementos_con_append__.py
@@ -0,0 +1,10 @@
+# Se crea una lista de colores con 10 elementos
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Se utilizan los métodos append() para añadir nuevos elementos al final de la lista
+# - colores.append('fuxia') añade el elemento 'fuxia' al final de la lista
+# - colores.append('celeste') añade el elemento 'celeste' al final de la lista
+colores.append('fuxia'), colores.append('celeste')
+
+# Se imprime la lista 'colores' después de añadir los nuevos elementos
+print(colores)
