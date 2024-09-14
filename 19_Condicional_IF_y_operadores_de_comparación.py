98d7fdcda06e382a922e0086d07a048efd8f0c4a
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:23:02 -0600

    Practica 19

--- 19_Condicional_IF_y_operadores_de_comparación.py
+++ 19_Condicional_IF_y_operadores_de_comparación.py
@@ -2,7 +2,7 @@
 num1 = 15
 num2 = 20
 
-# Se evalúa una condición: si num1 es menor que num2
+# Se evalÃºa una condiciÃ³n: si num1 es menor que num2
 # En este caso, 15 es menor que 20, por lo que se ejecuta el bloque del if
 if num1 < num2:
     print('Se ejecuta el if.')
@@ -11,7 +11,7 @@ if num1 < num2:
 num1 = 1450
 num2 = 60
 
-# Se evalúa una condición: si num1 es mayor que num2
+# Se evalÃºa una condiciÃ³n: si num1 es mayor que num2
 # En este caso, 1450 es mayor que 60, por lo que se ejecuta el bloque del if
 if num1 > num2:
     print('Se ejecuta el if.')
@@ -20,7 +20,7 @@ if num1 > num2:
 num1 = 1450
 num2 = 60
 
-# Se evalúa una condición: si num1 es igual a num2
+# Se evalÃºa una condiciÃ³n: si num1 es igual a num2
 # En este caso, 1450 no es igual a 60, por lo que el bloque del if no se ejecuta
 if num1 == num2:
     print('Se ejecuta el if.')
