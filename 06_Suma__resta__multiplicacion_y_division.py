aeee5357da17ae844779f58571cfa13dcd7cf986
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:16:59 -0600

    Practica 6

---
+++ 06_Suma__resta__multiplicacion_y_division.py
@@ -0,0 +1,17 @@
+# Se realiza la suma de 20, 23 y 44, y el resultado se asigna a la variable 'suma'
+suma = 20 + 23 + 44
+
+# Se realiza la resta de 20, 23 y 84, y el resultado se asigna a la variable 'resta'
+resta = 20 - 23 - 84
+
+# Se realiza la multiplicación de 10 por 87, y el resultado se asigna a la variable 'multiplicacion'
+multiplicacion = 10 * 87
+
+# Se realiza la división de 5000 entre 2, y luego el resultado se divide entre 230. El resultado se asigna a la variable 'division'
+division = 5000 / 2 / 230
+
+# Se realiza una operación combinada: 10 dividido entre 5, más 15, menos 17. El resultado se asigna a la variable 'operacion'
+operacion = 10 / 5 + 15 - 17
+
+# Se imprime el resultado de cada operación con el formato especificado
+print("Suma:", suma, "\nResta:", resta, "\nMultiplicacion:", multiplicacion, "\nDivision:", division, "\nOperacion:", operacion)
