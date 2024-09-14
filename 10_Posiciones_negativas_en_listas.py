7be22b2b25da43afe9a8206a8a41fc0d93bcdced
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 23:19:43 -0600

    Practica 10

---
+++ 10_Posiciones_negativas_en_listas.py
@@ -0,0 +1,16 @@
+
+# Se crea una lista de colores con 10 elementos
+colores = ['rojo','azul','verde','amarillo','marron','lila','negro','rosa','blanco','naranja']
+
+
+print('\n',colores[-1],'\n',colores[3],'\n',colores[-5],'\n',colores[-2],'\n',colores[0])
+
+# El índice -1 hace referencia al último elemento de la lista, que es 'naranja'
+
+# El índice 3 hace referencia al cuarto elemento de la lista, que es 'amarillo'
+
+# El índice -5 hace referencia al quinto elemento desde el final, que es 'lila'
+
+# El índice -2 hace referencia al penúltimo elemento, que es 'blanco'
+
+# El índice 0 hace referencia al primer elemento, que es 'rojo'
