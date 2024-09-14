f3dd996d6da6bffbccc6e5d58e16376dc2e0f44d
Jauregui-1 <jaredrj03@gmail.com>
2024-09-13 22:26:29 -0600

    Practica 9

---
+++ Listas.py
@@ -0,0 +1,18 @@
+# Se crea una lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa']
+
+# Se crean dos variables con preguntas para imprimir luego
+pregunta01 = 'Que color esta en la posicion 3: '
+pregunta02 = 'En que posicion se encuentra el color rojo y rosa: '
+
+# Se imprime la primera pregunta junto con el color que está en la posición 3 de la lista 'colores'
+# Las listas en Python empiezan en la posición 0, por lo que 'colores[3]' es 'amarillo'
+print(pregunta01, colores[3])
+
+# Se imprime la segunda pregunta indicando las posiciones del color 'rojo' y 'rosa'
+# 'rojo' está en la posición 0 y 'rosa' está en la posición 7
+print(pregunta02 + '0 ' + 'y ' + '7')
+
+# Se crea una lista de palabras que representan los numeros indicados en el ejercicio
+numeros = ['tres', 'dos', 'cinco', 'cuatro', 'uno']
+
