13450ad95db13052ffdf67fb9ed40fe1aae90271
Jauregui-1 <jaredrj03@gmail.com>
2024-09-14 18:51:26 -0600

    Practica 31

---
+++ 31_Crear_y_llamar_funciones.py
@@ -0,0 +1,15 @@
+# Se define una función llamada 'suma' que toma dos argumentos: 'num1' y 'num2'
+def suma(num1, num2):
+    # La variable 'resultado' almacena la suma de los dos números proporcionados como argumentos
+    resultado = num1 + num2
+    # Se imprime el resultado de la suma seguido de un salto de línea
+    print(resultado, "\n")
+
+# Se llama a la función 'suma' con los valores 20 y 10 como argumentos
+suma(20, 10)
+
+# Se llama a la función 'suma' con los valores 40 y 10 como argumentos
+suma(40, 10)
+
+# Se llama a la función 'suma' con los valores 58000 y -1000 como argumentos
+suma(58000, -1000)
